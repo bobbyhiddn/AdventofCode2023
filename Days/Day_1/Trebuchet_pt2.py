85619f6d7e5ed90a5b271798008ae9b77a1d0dc2
bobbyhiddn <mlmicahlongmire@gmail.com>
2023-12-03 20:54:48 +0000

    Solved day 1 part 2

--- Days/Day_1/Trebuchet_pt2.py
+++ Days/Day_1/Trebuchet_pt2.py
@@ -9,38 +9,34 @@ number_words = {
 def find_and_convert_numbers(s):
     # Extract words from the string that match our number_words keys
     words = re.findall(r'\b(' + '|'.join(number_words.keys()) + r')\b', s, flags=re.IGNORECASE)
-    
-    # Replace spelled out numbers with digits
-    for word in words:
-        s = s.replace(word, number_words[word.lower()])
-    
-    # Find all numbers in the modified string
-    return re.findall(r'\d+', s)
+
+    # Convert each word to its corresponding digit and join them together
+    number_string = ''.join(number_words[word.lower()] for word in words)
+
+    # Return the number string if it's not empty, else None
+    return number_string if number_string else None
 
 def concatenate_first_and_last_digit(s):
-    numbers = find_and_convert_numbers(s)
-    if not numbers:
+    number_string = find_and_convert_numbers(s)
+    if not number_string:
         return None  # No numbers found
-    # Take the first digit of the first number and the last digit of the last number
-    first_digit = numbers[0][0]
-    last_digit = numbers[-1][-1]
+    # Take the first and last digit
+    first_digit = number_string[0]
+    last_digit = number_string[-1]
     return int(first_digit + last_digit)
 
 # Read jumbled strings from 'Trebuchet.txt'
 with open('Trebuchet.bk.txt', 'r') as file:
     jumbled_strings = [line.strip() for line in file]
 
-# Initialize a variable to store the sum of all concatenated numbers
-total_sum = 0
+# Create a new list of concatenated numbers
+concatenated_numbers = [concatenate_first_and_last_digit(s) for s in jumbled_strings if concatenate_first_and_last_digit(s) is not None]
 
-# Processing each jumbled string
-for jumbled_string in jumbled_strings:
-    concatenated_number = concatenate_first_and_last_digit(jumbled_string)
-    if concatenated_number is not None:
-        print(f"Concatenated number in '{jumbled_string}': {concatenated_number}")
-        total_sum += concatenated_number
-    else:
-        print(f"No numbers found in '{jumbled_string}'")
+# Process and print each concatenated number
+for jumbled_string, concatenated_number in zip(jumbled_strings, concatenated_numbers):
+    print(f"Concatenated number in '{jumbled_string}': {concatenated_number}")
 
-# Print the total sum
+# Calculate and print the total sum
+total_sum = sum(concatenated_numbers)
 print(f"Total sum of concatenated numbers: {total_sum}")
+
