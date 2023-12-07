9fc6de0b374f0ca15f687753aeca7e45facd90c5
bobbyhiddn <mlmicahlongmire@gmail.com>
2023-12-06 21:32:52 -0700

    Solved

--- Days/Day_6/Race.py
+++ Days/Day_6/Race.py
@@ -23,14 +23,43 @@ import sys
 with open(sys.argv[1], 'r') as file:
     races = [line.strip() for line in file]
 
+def parse_races():
+    times = []
+    distances = []
+    # Assuming the first line in races is for times and the second for distances
+    if races:
+        times = list(map(int, races[0].replace("Time:", "").strip().split()))
+    if len(races) > 1:
+        distances = list(map(int, races[1].replace("Distance:", "").strip().split()))
+
+    # Pair each time with its corresponding distance
+    return list(zip(times, distances))
+
+def find_records(race_data):
+    all_records = []  # This will be a list of lists
+    # Process each race
+    for race_time, record_distance in race_data:
+        race_records = []  # List to hold records for the current race
+        distance = 0
+        for i in range(race_time):
+            distance = i * (race_time - i)
+            if distance > record_distance:
+                race_records.append(distance)  # Append the record to the current race's list
+        all_records.append(race_records)  # Append the current race's records to the overall list
+    return all_records
+
 
 def main():
-    # Create tuples from input
-    for race in races:
-        # Split the input into a list of tuples
-        split
-        print(race)
-
+    # Parse the races and find records
+    race_data = parse_races()
+    print(race_data)
+    records = find_records(race_data)
+    print(records)
+    answer = 1
+    for record in records:
+        answer *= len(record)
+        
+    print("Answer: ", answer)
 
 if __name__ == '__main__':
     main()
